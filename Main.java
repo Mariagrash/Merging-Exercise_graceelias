0bd182d338e94bb2852c9d4e6da73f1201b6ea06
Mariagrash <eliasgrasya123@gmail.com>
2022-10-18 13:15:34 +0800

    merge to see conflic

--- Main.java
+++ Main.java
@@ -1,17 +1,7 @@
-<<<<<<< HEAD
+
 public class Main {
     public static void main(String args) {
-        System.out.println("Hi I am Mary Grace Elias");
+        System.out.println("This is inside the another branch that could make a conflic");
 
     }
 }
-=======
-public class Main{
-       public static void main (String args){
-System.out.println("Merging-Exercise_graceelias");
-System.out.println("This is inside the second branch");
-
-       }
-}
-
->>>>>>> new_brach
